ce4145e15ec6f691089191d11408d83ee243b89e
shamim <shamim@disystem.net>
2024-09-25 10:00:04 +0600

    category

--- app/Http/Resources/ProductChildCategoryResource.php
+++ app/Http/Resources/ProductChildCategoryResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Request;
 
-class ProductCategoryResource extends JsonResource
+class ProductChildCategoryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -13,8 +13,6 @@ class ProductCategoryResource extends JsonResource
      * @param  Request  $request
      * @return array
      */
-
-    
 
     public function toArray($request)
     {
@@ -26,7 +24,7 @@ class ProductCategoryResource extends JsonResource
             'label' => $locales['category_name']['value'] ?? $this->category_name,
             'category_name' => $locales['category_name']['value'] ?? $this->category_name,
             'parent_id' => $this->parent_id,
-            'childrenRecursive' => ProductChildCategoryResource::collection($this->childrenRecursive),
+            'childrenRecursive' => $this->childrenRecursive,
             'category_slug' => $locales['category_slug']['value'] ?? $this->category_slug,
             'category_banner' => $this->getFirstMediaUrl('category_banner'), // Fetch the URL of the brand logo
             'category_cover_image' => $this->getFirstMediaUrl('category_cover_image'), // Fetch the URL of the brand logo
