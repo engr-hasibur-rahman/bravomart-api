f51373027e3376dc5961b2892507bb77cc58dcd1
Mehedi <mehedi@disystem.net>
2024-10-17 19:36:48 +0600

    Message

--- app/Helpers.php
+++ app/Helpers.php
@@ -9,16 +9,16 @@ if (! function_exists('translate')) {
             return trans($key, $replace);
         }
 
-        $key = strpos($key, 'messages.') === 0?substr($key,9):$key;
+        $key = strpos($key, 'notification.') === 0?substr($key,9):$key;
         $local = app()->getLocale();
         try {
-            $lang_array = include(base_path('resources/lang/' . $local . '/messages.php'));
+            $lang_array = include(base_path('resources/lang/' . $local . '/notifications.php'));
             $processed_key = ucfirst(str_replace('_', ' ', CommonHelper::remove_invalid_charcaters($key)));
 
             if (!array_key_exists($key, $lang_array)) {
                 $lang_array[$key] = $processed_key;
                 $str = "<?php return " . var_export($lang_array, true) . ";";
-                file_put_contents(base_path('resources/lang/' . $local . '/messages.php'), $str);
+                file_put_contents(base_path('resources/lang/' . $local . '/notifications.php'), $str);
                 $result = $processed_key;
             } else {
                 $result = trans('messages.' . $key, $replace);
