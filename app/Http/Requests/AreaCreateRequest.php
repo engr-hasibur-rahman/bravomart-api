7bd0da83956efec67e0e70bf1530e3b44e6417bb
Mehedi <mehedi@disystem.net>
2024-10-23 14:40:17 +0600

    Area Entry

---
+++ app/Http/Requests/AreaCreateRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AreaCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'     => ['required', 'string', 'max:255'],
+            'code' => 'required|unique:com_areas,code,' . $this->id
+        ];
+    }
+
+    /**
+     * Get the error messages that apply to the request parameters.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required'      => 'Name is required',
+            'name.string'        => 'Name is not a valid string',
+            'name.max:255'       => 'Name can not be more than 255 character',
+
+            'code.required'     => 'email is required',
+            'code.max:255'       => 'Name can not be more than 255 character',
+            'code.unique:com_areas' => 'email must be unique'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+}
