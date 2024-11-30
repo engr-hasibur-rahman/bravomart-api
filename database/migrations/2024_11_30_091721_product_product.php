21fd5d7120da3bb44e53fc4ff77476ae17cfd790
Mehedi <mehedi@disystem.net>
2024-11-30 20:40:31 +0600

    Product table

--- database/migrations/2024_11_30_091721_product_product.php
+++ database/migrations/2024_11_30_091721_product_product.php
@@ -14,32 +14,31 @@ return new class extends Migration
         Schema::create('product_product', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('shop_id');
-            $table->string('product_type')->nullable(); //medicine/ furniture/ DOOR/ FOOD/ GROCERY
-            $table->string('product_behaviour')->nullable(); //1. product 2. consu 3. combo 4. service
-            $table->string('product_name')->unique();
-            $table->string('product_description')->nullable();
-            $table->string('category_id')->nullable();
-            $table->string('product_brand_id')->nullable();
-            $table->string('product_uom_id')->nullable();
-            $table->string('product_uom_name')->nullable();
-            $table->string('product_class')->nullable(); //This will controll product attribute, different attribute for food/medicine/cloth
-            $table->string('product_warranty')->nullable(); //[ { "warranty_period": 5,    "warranty_text": " Years for Compressor"  }, { "warranty_period": 2,    "warranty_text": " Years for Spare Parts"  }]
+            $table->string('type')->nullable(); //medicine/ furniture/ DOOR/ FOOD/ GROCERY
+            $table->string('behaviour')->nullable(); //1. product 2. consu 3. combo 4. service
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('brand_id')->nullable();
+            $table->string('unit_name')->nullable();
+            $table->string('class')->nullable(); //This will controll product attribute, different attribute for food/medicine/cloth
+            $table->string('warranty')->nullable(); //[ { "warranty_period": 5,    "warranty_text": " Years for Compressor"  }, { "warranty_period": 2,    "warranty_text": " Years for Spare Parts"  }]
             $table->string('return_in_dsays')->nullable();
             $table->string('return_text')->nullable(); // Ex:  Days return Policy, Box Required
             $table->string('allow_change_in_mind')->nullable();
-            $table->string('cash_on_delivery')->nullable(); //0=Not available 100=Available 40=60% advance and rest amount is Cash on Delevery
+            $table->integer('cash_on_delivery')->nullable(); //0=Not available 100=Available 40=60% advance and rest amount is Cash on Delevery
             $table->string('delivery_time_min')->nullable();
             $table->string('delivery_time_max')->nullable();
             $table->string('delivery_time_text')->nullable(); //*Can be delay if natural disaster
-            $table->string('product_tags')->nullable();
-            $table->string('product_thumb')->nullable();
-            $table->string('product_image')->nullable();
+            $table->string('tags')->nullable();
+            $table->string('thumb_image')->nullable();
+            $table->string('full_image')->nullable();
             $table->string('product_slug')->nullable();
             $table->string('status')->nullable(); //pending, approved, inactive, suspended
-            $table->string('maximum_cart_qty')->nullable();
-            $table->string('rating_count')->nullable();
-            $table->string('rating_average')->nullable();
-            $table->string('order_count')->nullable();
+            $table->integer('max_cart_qty')->nullable();
+            $table->integer('rating_count')->nullable();
+            $table->decimal('rating_average')->nullable();
+            $table->integer('order_count')->nullable();
             $table->string('attributes')->nullable();
             $table->timestamp('available_time_starts')->nullable(); //Only for Food Item
             $table->timestamp('available_time_ends')->nullable(); //Only for Food Item
--- database/migrations/2024_11_30_091807_product_product_variant.php
+++ database/migrations/2024_11_30_091807_product_product_variant.php
@@ -13,15 +13,16 @@ return new class extends Migration
     {
         Schema::create('product_product_variant', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->foreign('product_id')->references('id')->on('product_product')->onDelete('cascade');
             $table->string('variant_slug')->nullable();
             $table->string('sku')->nullable();
-            $table->string('pack_quantity')->nullable();
-            $table->string('weight_major')->unique();
-            $table->string('weight_gross')->nullable();
-            $table->string('weight_net')->nullable();
+            $table->decimal('pack_quantity')->nullable();
+            $table->decimal('weight_major')->nullable();
+            $table->decimal('weight_gross')->nullable();
+            $table->decimal('weight_net')->nullable();
             $table->string('variant_image')->nullable(); //[{"sliding_image":"xyx.jpg","position":1},{"sliding_image":"abc.jpg","position":2}]
-            $table->string('variant_order_count')->nullable();
+            $table->integer('variant_order_count')->nullable();
             $table->string('status')->nullable(); //1. Active, 2. Inactive or empty
             $table->timestamps();
         });
