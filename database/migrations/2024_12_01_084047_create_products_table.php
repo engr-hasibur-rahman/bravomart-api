fc683e2701a1fb639ff3ab0f4f62a8ba3dec809c
shamim <shamim@disystem.net>
2024-12-01 16:39:57 +0600

    product table modified

--- database/migrations/2024_12_01_084047_create_products_table.php
+++ database/migrations/2024_12_01_084047_create_products_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\StatusType;
+use App\Enums\StoreType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,13 +19,16 @@ return new class extends Migration
             $table->unsignedBigInteger('category_id')->nullable();
             $table->unsignedBigInteger('brand_id')->nullable();
             $table->unsignedBigInteger('unit_id')->nullable();
-            $table->string('attribute_id')->nullable();
-            $table->string('type')->nullable();
+            $table->unsignedBigInteger('attribute_id')->nullable();
+            $table->unsignedBigInteger('tag_id')->nullable();
+            $table->enum('type', array_map(fn($enum) => $enum->value, StoreType::cases()))->nullable();
             $table->string('behaviour')->nullable();
             $table->string('name')->unique();
+            $table->string('slug')->nullable();
             $table->longText('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('gallery_images')->nullable();
             $table->string('warranty')->nullable();
-            //$table->enum('type', ['medicine', 'furniture', 'food', 'grocery', 'service'])->nullable();
             $table->string('return_in_dsays')->nullable();
             $table->string('return_text')->nullable();
             $table->string('allow_change_in_mind')->nullable();
@@ -31,15 +36,11 @@ return new class extends Migration
             $table->string('delivery_time_min')->nullable();
             $table->string('delivery_time_max')->nullable();
             $table->string('delivery_time_text')->nullable();
-            $table->string('tags')->nullable();
-            $table->string('thumb_image')->nullable();
-            $table->string('full_image')->nullable();
-            $table->string('slug')->nullable();
             $table->integer('max_cart_qty')->nullable();
             $table->integer('order_count')->nullable();
             $table->string('attributes')->nullable();
             $table->integer('views')->default(0);
-            $table->enum('status', ['draft','pending', 'approved', 'inactive', 'suspended'])->default('draft');
+           $table->enum('status', array_map(fn($enum) => $enum->value, StatusType::cases()))->nullable();
             $table->timestamp('available_time_starts')->nullable();
             $table->timestamp('available_time_ends')->nullable();
             $table->timestamps();
