d38e3161258b6b6a58ab08da52a64aab35ebd186
Faysal <jesan@disystem.net>
2024-12-08 17:51:06 +0600

    MODIFIED: Store Management

--- app/Http/Controllers/Api/V1/Com/StoreManageController.php
+++ app/Http/Controllers/Api/V1/Com/StoreManageController.php
@@ -24,8 +24,7 @@ class StoreManageController extends Controller
         );
     }
     public function store(StoreRequest $request): JsonResponse
-    {
-        dd($request->all());
+    {       
         $store = $this->storeRepo->store($request->all());
         $this->storeRepo->storeTranslation($request, $store, 'App\Models\ComStore', $this->storeRepo->translationKeys());
         if ($store) {
@@ -52,5 +51,12 @@ class StoreManageController extends Controller
     {
         $this->storeRepo->delete($id);
         return $this->success(translate('messages.delete_success'));
+    }
+    public function deleted_records(){
+        $records = $this->storeRepo->records(true);
+        return response ()->json([
+            "data"=> $records,
+            "massage" => "Records were restored successfully!"
+        ],201);
     }
 }
