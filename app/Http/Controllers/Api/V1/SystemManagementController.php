a83bf94af6e1fbaabb44f62335ddf24486379526
Hasibur <hasibur@disystem.net>
2024-12-08 17:10:21 +0600

    Modified: system management general settings

--- app/Http/Controllers/Api/V1/SystemManagementController.php
+++ app/Http/Controllers/Api/V1/SystemManagementController.php
@@ -5,16 +5,24 @@ namespace App\Http\Controllers\Api\V1;
 use App\Actions\ImageModifier;
 use App\Http\Controllers\Controller;
 use App\Interfaces\TranslationInterface;
+use App\Models\ComOption;
 use App\Models\SystemManagement;
 use Illuminate\Http\Request;
 
 class SystemManagementController extends Controller
 {
-    public function __construct(protected TranslationInterface $translationRepository) {}
+    public function __construct(
+        protected TranslationInterface $transRepo,
+        protected ComOption $get_com_option,
+    ) {}
+
+    public function translationKeys(): mixed
+    {
+        return $this->get_com_option->translationKeys;
+    }
 
     public function generalSettings(Request $request){
         if ($request->isMethod('POST')) {
-
             $this->validate($request, [
                 'com_site_logo' => 'nullable|string',
                 'com_site_favicon' => 'nullable|string',
@@ -26,29 +34,19 @@ class SystemManagementController extends Controller
             ]);
 
             $fields = ['com_site_logo', 'com_site_favicon', 'com_site_title', 'com_site_subtitle', 'com_user_email_verification', 'com_user_login_otp', 'com_maintenance_mode'];
+
             foreach ($fields as $field) {
                   $value = $request->input($field) ?? null;
                   com_option_update($field, $value);
-
             }
-
 
             // Define the fields that need to be translated
             $fields = ['com_site_title', 'com_site_subtitle'];
-            $translationsData = [];
-            foreach ($fields as $field) {
-                $translationKey = $field . '_translation';
-                if ($request->has($translationKey)) {
-                    $translationsData[$field] = $request->input($translationKey);
-                }
-            }
+            $com_options = ComOption::whereIn('option_name', $fields)->get(['id']);
 
-            $this->translationRepository->storeTranslation(
-                $request,
-                $translationsData,
-                'App\Models\ComOption',
-                $this->translationRepository->translationKeys()
-            );
+            foreach ($com_options as $com_option) {
+                $this->transRepo->storeTranslation($request, $com_option->id, 'App\Models\ComOption', $this->translationKeys());
+            }
 
             return $this->success(translate('messages.update_success', ['name' => 'General Settings']));
         }else{
