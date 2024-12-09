377d02712d9021bfb68689b1cebbf65462dbfaff
Hasibur <hasibur@disystem.net>
2024-12-09 16:27:02 +0600

    seo settings added

--- app/Http/Controllers/Api/V1/SystemManagementController.php
+++ app/Http/Controllers/Api/V1/SystemManagementController.php
@@ -95,4 +95,79 @@ class SystemManagementController extends Controller
         }
 
     }
+
+    public function seoSettings(Request $request){
+        if ($request->isMethod('POST')) {
+            $this->validate($request, [
+                'com_meta_title' => 'nullable|string',
+                'com_meta_description' => 'nullable|string',
+                'com_meta_tags' => 'nullable|string',
+                'com_canonical_url' => 'nullable|string',
+                'com_og_title' => 'nullable|string',
+                'com_og_description' => 'nullable|string',
+                'com_og_image' => 'nullable|string',
+            ]);
+
+            $fields = ['com_meta_title', 'com_meta_description', 'com_meta_tags', 'com_canonical_url', 'com_og_title', 'com_og_description', 'com_og_image'];
+
+            foreach ($fields as $field) {
+                  $value = $request->input($field) ?? null;
+                  com_option_update($field, $value);
+            }
+
+            // Define the fields that need to be translated
+            $fields = ['com_meta_title', 'com_meta_description', 'com_meta_tags','com_og_title', 'com_og_description'];
+            $com_options = ComOption::whereIn('option_name', $fields)->get(['id']);
+
+            foreach ($com_options as $com_option) {
+                $this->transRepo->storeTranslation($request, $com_option->id, 'App\Models\ComOption', $this->translationKeys());
+            }
+
+            return $this->success(translate('messages.update_success', ['name' => 'General Settings']));
+        }else{
+            // Create an instance of ImageModifier
+            $imageModifier = new ImageModifier();
+
+            $ComOptionGet = ComOption::with('translations')->whereIn('option_name', ['com_site_title', 'com_site_subtitle'])
+                ->get(['id']);
+
+            // transformed data
+            $transformedData = [];
+            foreach ($ComOptionGet as $com_option) {
+                $translations = $com_option->translations()->get()->groupBy('language');
+                foreach ($translations as $language => $items) {
+                    $languageInfo = ['language' => $language];
+                    /* iterate all Column to Assign Language Value */
+                    foreach ($this->get_com_option->translationKeys as $columnName) {
+                        $languageInfo[$columnName] = $items->where('key', $columnName)->first()->value ?? "";
+                    }
+                    $transformedData[] = $languageInfo;
+                }
+            }
+
+            $com_site_logo = com_option_get('com_site_logo');
+            $com_site_favicon = com_option_get('com_site_favicon');
+            $com_site_logo_image_url = $imageModifier->generateImageUrl(com_option_get('com_site_logo'));
+            $com_site_favicon_image_url = $imageModifier->generateImageUrl(com_option_get('com_site_favicon'));
+            $com_site_title = com_option_get('com_site_title') ?? '';
+            $com_site_subtitle = com_option_get('com_site_subtitle') ?? '';
+            $com_user_email_verification = com_option_get('com_user_email_verification') ?? '';
+            $com_user_login_otp = com_option_get('com_user_login_otp') ?? '';
+            $com_maintenance_mode = com_option_get('com_maintenance_mode') ?? '';
+
+            return $this->success([
+                'com_site_logo' => $com_site_logo,
+                'com_site_favicon' => $com_site_favicon,
+                'com_site_logo_image_url' => $com_site_logo_image_url,
+                'com_site_favicon_image_url' => $com_site_favicon_image_url,
+                'com_site_title' => $com_site_title,
+                'com_site_subtitle' => $com_site_subtitle,
+                'com_user_email_verification' => $com_user_email_verification,
+                'com_user_login_otp' => $com_user_login_otp,
+                'com_maintenance_mode' => $com_maintenance_mode,
+                'translations' => $transformedData,
+            ]);
+        }
+
+    }
 }
