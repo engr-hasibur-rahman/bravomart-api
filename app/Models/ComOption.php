35839a47a9fcc6afa8078028d989eaa9899016b7
Hasibur <hasibur@disystem.net>
2024-12-10 13:00:34 +0600

    Modified: General Settings

--- app/Models/ComOption.php
+++ app/Models/ComOption.php
@@ -17,7 +17,7 @@ class ComOption extends Model
 
     public $translationKeys = [
         'option_name','com_site_title','com_site_subtitle','com_meta_title', 'com_meta_description', 'com_meta_tags','com_og_title', 'com_og_description',
-        'com_maintenance_title', 'com_maintenance_description',
+        'com_maintenance_title', 'com_maintenance_description', 'com_site_full_address','com_site_contact_number', 'com_site_footer_copyright',
     ];
     public function translations()
     {
