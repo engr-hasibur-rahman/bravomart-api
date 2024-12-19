723aa21df894657584b44c3833c31c618dc163f7
Faysal <jesan@disystem.net>
2024-12-19 12:28:23 +0600

    ADDED: Slider and Banner Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionStoreSeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(LocationSeeder::class);
+        $this->call(SliderSeeder::class);
+        $this->call(BannerSeeder::class);
     }
 }
