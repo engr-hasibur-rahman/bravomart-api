707efb75ea409f0f569e344f1196053c726a1820
Mehedi <mehedi@disystem.net>
2024-12-24 19:12:13 +0600

    Order Table

--- database/migrations/2024_12_23_053228_create_orders_table.php
+++ database/migrations/2024_12_23_053228_create_orders_table.php
@@ -13,15 +13,38 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('store_id');
-            $table->unsignedBigInteger('customer_id');
-            $table->unsignedBigInteger('area_id');
-            $table->string('shipping_type');
-            $table->string('shipping_address');
-            $table->string('shipping_time_preferred');
-            $table->string('delivery_status');
-            $table->string('payment_type');
-            $table->string('payment_status');
+            $table->unsignedBigInteger('store_id')->nullable();
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->string('shipping_type')->nullable();
+            $table->string('shipping_address_id')->nullable();
+            $table->string('shipping_time_preferred')->nullable();
+            $table->string('delivery_status')->nullable();
+            $table->string('payment_type')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->string('order_notes')->nullable();
+
+            $table->decimal('order_amount')->nullable();
+            $table->string('coupon_code')->nullable();
+            $table->string('coupon_title')->nullable();
+            $table->decimal('coupon_disc_amt_admin')->nullable();
+            $table->decimal('coupon_disc_amt_store')->nullable();
+
+            $table->decimal('product_disc_amt')->nullable();
+            $table->decimal('flash_disc_amt_admin')->nullable();
+            $table->decimal('flash_disc_amt_store')->nullable();
+            $table->decimal('shipping_charge')->nullable();
+            $table->decimal('additional_charge')->nullable();
+
+
+            $table->timestamp('confirmed_at')->nullable();
+            $table->timestamp('cancel_request_at')->nullable();
+            $table->timestamp('cancelled_at')->nullable();
+
+            //$table->string('delivery_status')->nullable();
+            $table->timestamp('delivery_completed_at')->nullable();
+
+            $table->string('refund_status')->nullable();
 
             $table->timestamps();
         });
