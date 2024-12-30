6785863fcaa3fe7dd107a7a478da34224de36175
Mehedi <mehedi@disystem.net>
2024-12-30 17:54:25 +0600

    Order Table

--- database/migrations/2024_12_23_053229_create_order_packages_table.php
+++ database/migrations/2024_12_23_053229_create_order_packages_table.php
@@ -13,7 +13,30 @@ return new class extends Migration
     {
         Schema::create('order_packages', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->unsignedBigInteger('store_id')->nullable();
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->string('shipping_type')->nullable();
+            // Keeping Duplicate Column in Order & Store table. Due to our complex order structure, Here we will kep Store wise Summary To Show in his cart. Main Order Table will store Summarized data
+            $table->decimal('order_amount')->nullable();
+            $table->string('coupon_code')->nullable();
+            $table->string('coupon_title')->nullable();
+            $table->decimal('coupon_disc_amt_admin')->nullable();
+            $table->decimal('coupon_disc_amt_store')->nullable();
+
+            $table->decimal('product_disc_amt')->nullable();
+            $table->decimal('flash_disc_amt_admin')->nullable();
+            $table->decimal('flash_disc_amt_store')->nullable();
+            $table->decimal('shipping_charge')->nullable();
+            $table->decimal('additional_charge')->nullable();
+
+
             $table->timestamps();
+        });
+        Schema::table('order_packages', function($table) {
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('store_id')->references('id')->on('com_merchant_stores')->onDelete('cascade');
+            $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
         });
     }
 
@@ -22,6 +45,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('order_packages');
+        Schema::dropIfExists('order_stores');
     }
 };
--- database/migrations/2024_12_23_053231_create_order_details_table.php
+++ database/migrations/2024_12_23_053231_create_order_details_table.php
@@ -13,7 +13,22 @@ return new class extends Migration
     {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->unsignedBigInteger('store_id')->nullable();
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->unsignedBigInteger('package_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->unsignedBigInteger('product_sku')->nullable();
+            $table->unsignedBigInteger('product_details')->nullable();
+            $table->unsignedBigInteger('variation_details')->nullable();
+
             $table->timestamps();
+        });
+
+        Schema::table('order_details', function($table) {
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('store_id')->references('id')->on('com_merchant_stores')->onDelete('cascade');
+            $table->foreign('area_id')->references('id')->on('areas')->onDelete('cascade');
         });
     }
 
