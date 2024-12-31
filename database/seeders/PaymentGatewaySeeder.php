ae0347300cf85dc329a1c30d85222835867bb432
Hasibur <hasibur@disystem.net>
2024-12-31 15:46:56 +0600

    Added: payment geteway seeder

---
+++ database/seeders/PaymentGatewaySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Modules\PaymentGateways\app\Models\PaymentGateway;
+
+class PaymentGatewaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        PaymentGateway::create([
+            'name' => 'paypal',
+            'description' => 'fdf',
+            'auth_credentials' => json_encode([
+                'paypal_sandbox_client_id' => 'your-paypal-client-id',
+                'paypal_sandbox_client_secret' => 'your-paypal-client-secret',
+                'paypal_sandbox_client_app_id' => 'sandbox', // or "live"
+            ]),
+            'image' => '100',
+            'status' => true,
+            'is_test_mode' => true,
+        ]);
+    }
+}
