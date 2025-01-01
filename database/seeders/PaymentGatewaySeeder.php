54c03eea6006299d07422cca927b2d689311dda2
Hasibur <hasibur@disystem.net>
2025-01-01 14:35:22 +0600

    payment seeder issue fixed

--- database/seeders/PaymentGatewaySeeder.php
+++ database/seeders/PaymentGatewaySeeder.php
@@ -28,5 +28,19 @@ class PaymentGatewaySeeder extends Seeder
             'status' => true,
             'is_test_mode' => true,
         ]);
+
+        PaymentGateway::create([
+            'name' => 'paypal',
+            'slug' => 'paypal', // Add a slug value
+            'description' => 'fdf',
+            'auth_credentials' => json_encode([
+                'paypal_sandbox_client_id' => 'your-paypal-client-id',
+                'paypal_sandbox_client_secret' => 'your-paypal-client-secret',
+                'paypal_sandbox_client_app_id' => 'sandbox', // or "live"
+            ]),
+            'image' => '100',
+            'status' => true,
+            'is_test_mode' => true,
+        ]);
     }
 }
