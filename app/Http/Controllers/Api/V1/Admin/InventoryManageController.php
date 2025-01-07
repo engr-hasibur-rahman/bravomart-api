5a61c516b46125d997675acd0d6b877adf9997c4
Faysal <jesan@disystem.net>
2025-01-07 14:15:16 +0600

    MODIFIED: ProductController

--- app/Http/Controllers/Api/V1/Admin/InventoryManageController.php
+++ app/Http/Controllers/Api/V1/Admin/InventoryManageController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\InventoryRepositoryInterface;
 use Illuminate\Http\Request;
 
 class InventoryManageController extends Controller
 {
-    //
+    public function __construct(protected InventoryRepositoryInterface $inventoryRepo)
+    {
+
+    }
+
+    public function index()
+    {
+        $inventories = $this->inventoryRepo->getInventories();
+        return response()->json($inventories);
+    }
 }
---
+++ app/Interfaces/InventoryRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Interfaces;
+interface InventoryRepositoryInterface
+{
+ public function getInventories();
+}
