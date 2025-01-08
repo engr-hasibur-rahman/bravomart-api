d66c177dbe998b4da34af50bdf605b98dcd12230
Faysal <jesan@disystem.net>
2025-01-08 17:47:04 +0600

    MODIFIED: ProductDetailsResource

--- app/Http/Controllers/Api/V1/Admin/AdminStoreManageController.php
+++ app/Http/Controllers/Api/V1/Admin/AdminStoreManageController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\SellerWiseStoreForDropdownResource;
+use App\Http\Resources\Com\Pagination\PaginationResource;
+use App\Http\Resources\Com\Store\StoreListResource;
 use App\Interfaces\StoreManageInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -13,6 +15,24 @@ class AdminStoreManageController extends Controller
     public function __construct(protected StoreManageInterface $storeRepo)
     {
 
+    }
+
+    public function index(Request $request)
+    {
+        $stores = $this->storeRepo->getAllStores(
+            $request->limit ?? 10,
+            $request->page ?? 1,
+            $request->language ?? DEFAULT_LANGUAGE,
+            $request->search ?? "",
+            $request->sortField ?? 'id',
+            $request->sort ?? 'asc',
+            []
+        );
+        // Return the stores as a resource collection
+        return response()->json([
+            'data' => StoreListResource::collection($stores),
+            'meta' => new PaginationResource($stores),
+        ]);
     }
 
     public function sellerStores(Request $request)
