aa23f049ffd1ce23f73bac2d91261b1f7cfa5cbe
Hasibur <hasibur@disystem.net>
2025-01-16 10:33:07 +0600

    subscription module added

--- app/Services/StoreManageService.php
+++ app/Services/StoreManageService.php
@@ -5,25 +5,26 @@ namespace App\Services;
 use App\Jobs\SendStoreCreatedEmailJob;
 use App\Models\ComMerchantStore;
 use App\Models\SystemCommission;
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
-use Mockery\Exception;
-use Modules\Subscription\app\Models\ComMerchantStoresSubscription;
-use Modules\Subscription\app\Models\Subscription;
-use Modules\Subscription\app\Models\SubscriptionHistory;
+use Modules\Subscription\app\Services\SubscriptionService;
 
 class StoreManageService
 {
+
+    protected $subscriptionService;
+    public function __construct(SubscriptionService $subscriptionService = null)
+    {
+        // Only assign SubscriptionService if the module exists and is active
+        if (moduleExistsAndStatus('Subscription')) {
+            $this->subscriptionService = $subscriptionService;
+        }
+    }
 
     public function storeForAuthSeller($data){
 
             $data = Arr::except($data, ['translations']);
             $data['merchant_id'] = auth('api')->id();
             $store = ComMerchantStore::create($data);
-
 
             // store create after commission set
                $store = ComMerchantStore::find($store->id);
@@ -41,83 +42,14 @@ class StoreManageService
                     dispatch(new SendStoreCreatedEmailJob($store));
                 } catch (\Exception $exception) {
                 }
-
                 return $store_id;
-
-            }elseif(isset($store->subscription_type) && $store->subscription_type === 'subscription'){
-                // subscription package
-                $subscription_package = Subscription::where('id', $data['subscription_id'])
-                    ->where('status', 1)
-                    ->first();
-
-                // if package not found
-                if (empty($subscription_package)){
-                    return [
-                        'success' => false,
-                        'message' => "Subscription package not found",
-                    ];
-                }
-
-                // if seller store payment using wallet
-                 $payment_gateway = $data['payment_gateway'];
-                if(isset($payment_gateway) && $payment_gateway == 'wallet'){
-                    // find store and update store status
-                    $store = ComMerchantStore::find($store_id);
-                    $store->status = 1;
-                    $store->save();
-                    // subscription status set
-                    $subscription_status = 1;
-                    $payment_status = 'paid';
-                }else{
-                    $subscription_status = 0;
-                    $payment_status = 'pending';
-                }
-
-
-                // Create subscription history
-                SubscriptionHistory::create([
-                    'store_id' => $store_id,
-                    'subscription_id' => $subscription_package->id,
-                    'name' => $subscription_package->name,
-                    'validity' => $subscription_package->validity,
-                    'price' => $subscription_package->price,
-                    'pos_system' => $subscription_package->pos_system,
-                    'self_delivery' => $subscription_package->self_delivery,
-                    'mobile_app' => $subscription_package->mobile_app,
-                    'live_chat' => $subscription_package->live_chat,
-                    'order_limit' => $subscription_package->order_limit,
-                    'product_limit' => $subscription_package->product_limit,
-                    'product_featured_limit' => $subscription_package->product_featured_limit,
-                    'payment_gateway' =>$payment_gateway ?? null,
-                    'payment_status' => $payment_status ?? null,
-                    'transaction_id' => $data['transaction_id'] ?? null,
-                    'manual_image' => $data['manual_image'] ?? null,
-                    'expire_date' => now()->addDays($subscription_package->validity),
-                    'status' => $subscription_status,
-                ]);
-
-                // Create store wise subscription
-                ComMerchantStoresSubscription::create([
-                    'store_id' => $store_id,
-                    'subscription_id' => $subscription_package->id,
-                    'name' => $subscription_package->name,
-                    'validity' => $subscription_package->validity,
-                    'price' => $subscription_package->price,
-                    'pos_system' => $subscription_package->pos_system,
-                    'self_delivery' => $subscription_package->self_delivery,
-                    'mobile_app' => $subscription_package->mobile_app,
-                    'live_chat' => $subscription_package->live_chat,
-                    'order_limit' => $subscription_package->order_limit,
-                    'product_limit' => $subscription_package->product_limit,
-                    'product_featured_limit' => $subscription_package->product_featured_limit,
-                    'payment_gateway' => $payment_gateway ?? null,
-                    'payment_status' => $payment_status ?? null,
-                    'transaction_id' => $data['transaction_id'] ?? null,
-                    'manual_image' => $data['manual_image'] ?? null,
-                    'expire_date' => now()->addDays($subscription_package->validity),
-                    'status' => $subscription_status,
-                ]);
             }
+
+            // Store Subscription handle logic
+            if(moduleExistsAndStatus('Subscription')){
+                $this->subscriptionService->buySubscriptionPackage($store, $data);
+            }
+
             return $store_id;
 
     }
