1436e48c06a44150ab668ada65fb72385091bdef
Hasibur <hasibur@disystem.net>
2025-01-21 12:27:04 +0600

    admin order lists added

---
+++ app/Http/Controllers/Api/V1/Admin/AdminOrderManageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Com\Pagination\PaginationResource;
+use App\Http\Resources\Order\AdminOrderResource;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class AdminOrderManageController extends Controller
+{
+    public function allOrders(Request $request){
+
+        $store_id = $request->store_id;
+
+        $orders = Order::orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return response()->json([
+            'orders' => AdminOrderResource::collection($orders),
+            'meta' => new PaginationResource($orders)
+        ]);
+    }
+}
--- app/Http/Controllers/Api/V1/Seller/SellerStoreOrderController.php
+++ app/Http/Controllers/Api/V1/Seller/SellerStoreOrderController.php
@@ -6,14 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Com\Pagination\PaginationResource;
 use App\Http\Resources\Order\SellerStoreOrderResource;
 use App\Models\Order;
+use App\Models\OrderPackage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SellerStoreOrderController extends Controller
 {
     public function allOrders(Request $request){
-        $seller_id = auth()->guard('api_customer')->user()->id;
-        $orders = Order::where('seller_id', $seller_id)
-            ->where('store_id', $request->store_id)
+
+       $validator = Validator::make($request->all(), [
+            'store_id' => 'required|exists:com_merchant_stores,id',
+        ]);
+
+        // Check for validation failure
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ], 422);
+        }
+
+        $seller_id = auth()->guard('api')->user()->id;
+
+        $orders = Order::where('store_id', $request->store_id)
             ->orderBy('created_at', 'desc')
             ->paginate(10);
 
