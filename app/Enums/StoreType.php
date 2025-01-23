15ae18e22f271c36c4414f0f18c45f30ba6d8a39
Faysal <jesan@disystem.net>
2025-01-23 16:22:08 +0600

    Popular product and storetype with image

--- app/Enums/StoreType.php
+++ app/Enums/StoreType.php
@@ -13,6 +13,28 @@ enum StoreType: string
     case FURNITURE = 'furniture';
     case BOOKS = 'books';
     case GADGET = 'gadgets';
-    case ANIMALS_PET = 'animals-pet' ;
+    case ANIMALS_PET = 'animals-pet';
     case FISH = 'fish';
+
+    public static function images(): array
+    {
+        return [
+            self::GROCERY->value => asset('asset/grocery.jpg'),
+            self::BAKERY->value => 'https://example.com/images/electronics.png',
+            self::MEDICINE->value => 'https://example.com/images/fashion.png',
+            self::MAKEUP->value => 'https://example.com/images/fashion.png',
+            self::BAGS->value => 'https://example.com/images/fashion.png',
+            self::CLOTHING->value => 'https://example.com/images/fashion.png',
+            self::BOOKS->value => 'https://example.com/images/fashion.png',
+            self::GADGET->value => 'https://example.com/images/fashion.png',
+            self::ANIMALS_PET->value => 'https://example.com/images/fashion.png',
+            self::FISH->value => 'https://example.com/images/fashion.png',
+        ];
+    }
+
+    public function image(): string
+    {
+        return self::images()[$this->value] ?? 'https://example.com/images/default.png';
+    }
 }
+
