27b65a8c5277fa546929a0499cec41776856e1e1
Faysal <jesan@disystem.net>
2025-01-28 10:43:01 +0600

    ADDED: Trending Products

---
+++ app/Http/Resources/Product/TrendingProductPublicResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use App\Actions\ImageModifier;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrendingProductPublicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'store' => $this->store->name,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'image' => ImageModifier::generateImageUrl($this->image),
+            'stock' => $this->variants->isNotEmpty() ? $this->variants->sum('stock_quantity') : null,
+            'price' => optional($this->variants->first())->price,
+            'special_price' => optional($this->variants->first())->special_price,
+            'singleVariant' => $this->variants->count() === 1 ? [$this->variants->first()] : [],
+            'discount_percentage' => $this->variants->isNotEmpty() && optional($this->variants->first())->price > 0
+                ? round(((optional($this->variants->first())->price - optional($this->variants->first())->special_price) / optional($this->variants->first())->price) * 100, 2)
+                : null,
+            'wishlist' => auth('api_customer')->check() ? $this->wishlist : false, // Check if the customer is logged in,
+        ];
+    }
+}
