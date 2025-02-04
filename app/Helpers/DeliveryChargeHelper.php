6f2c997ad33b8e2f8470aabda99a692881d2f7ff
Hasibur <hasibur@disystem.net>
2025-02-04 16:25:11 +0600

    Fixed: shipping amout

--- app/Helpers/DeliveryChargeHelper.php
+++ app/Helpers/DeliveryChargeHelper.php
@@ -82,8 +82,11 @@ class DeliveryChargeHelper
             }
         }
 
+        dd($delivery_charge, $delivery_charge);
+
         // Ensure minimum delivery fee
         $delivery_charge = max($settings->min_order_delivery_fee, $delivery_charge);
+
 
         return [
             'delivery_method' => $settings->delivery_charge_method,
