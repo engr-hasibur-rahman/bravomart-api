66da35bd929600242657a4e372d8491d4c6453f7
Faysal <jesan@disystem.net>
2025-02-23 14:57:24 +0600

    MODIFIED: WalletCommonController

--- app/Interfaces/AdminDashboardManageInterface.php
+++ app/Interfaces/AdminDashboardManageInterface.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 interface AdminDashboardManageInterface
 {
     public function getSummaryData();
+
+    public function getSalesSummaryData(array $filters);
+
+    public function getOtherSummaryData();
 }
