688c386ee970567516c98d2b934f92ae51e1a731
Faysal <jesan@disystem.net>
2025-03-04 11:26:48 +0600

    MODIFIED: Frontend Controller

---
+++ app/Http/Resources/Com/Blog/BlogCategoryPublicResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources\Com\Blog;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BlogCategoryPublicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray(Request $request): array
+    {
+        // Get the requested language from the query parameter
+        $language = $request->input('language', 'en');
+        // Get the translation for the requested language
+        $translation = $this->related_translations->where('language', $language);
+        return [
+            'id' => $this->id,
+            'name' => !empty($translation) && $translation->where('key', 'name')->first()
+                ? $translation->where('key', 'name')->first()->value
+                : $this->name, // If language is empty or not provided attribute
+            'slug' => $this->slug,
+            'meta_title' => !empty($translation) && $translation->where('key', 'meta_title')->first()
+                ? $translation->where('key', 'meta_title')->first()->value
+                : $this->meta_title, // If language is empty or not provided attribute
+            'meta_description' => !empty($translation) && $translation->where('key', 'meta_description')->first()
+                ? $translation->where('key', 'meta_description')->first()->value
+                : $this->meta_description, // If language is empty or not provided attribute
+        ];
+    }
+}
