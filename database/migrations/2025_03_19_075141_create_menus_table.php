a731bf79ec21451a52334c981ee021bbfb1389c7
Faysal <jesan@disystem.net>
2025-04-07 16:10:21 +0600

    MODIFIED: Permission key

--- database/migrations/2025_03_19_075141_create_menus_table.php
+++ database/migrations/2025_03_19_075141_create_menus_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('url');
+            $table->string('url')->nullable();
             $table->string('icon')->nullable();
             $table->integer('position')->default(0);
             $table->unsignedBigInteger('parent_id')->nullable(); // For nesting
