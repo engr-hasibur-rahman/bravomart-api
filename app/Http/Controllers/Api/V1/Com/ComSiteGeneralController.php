68765cc7653c8bb19bd0ecd03021cb85e11abfe6
Hasibur <hasibur@disystem.net>
2025-04-15 12:40:11 +0600

    logo issue fixed

--- app/Http/Controllers/Api/V1/Com/ComSiteGeneralController.php
+++ app/Http/Controllers/Api/V1/Com/ComSiteGeneralController.php
@@ -12,29 +12,40 @@ class ComSiteGeneralController extends Controller
     public function siteGeneralInfo(Request $request){
 
         $filter_data = $request->filter;
-        if ($filter_data == 'logo') {
+
+        $allowed_filters = [
+            'logo' => 'com_site_logo',
+            'white-logo' => 'com_site_white_logo',
+            'favicon' => 'com_site_favicon',
+            'maintenance' => 'com_maintenance_mode',
+            'email-verification' => 'com_user_email_verification',
+        ];
+
+        if (array_key_exists($filter_data, $allowed_filters)) {
+            $key = $allowed_filters[$filter_data];
             $site_settings = [
-                'com_site_logo' => com_option_get('com_site_logo'),
+                $key => com_option_get('com_site_' . $key),
             ];
             return response()->json([
-                'site_settings' => New SiteGeneralInfoFilterResource($site_settings),
+                'site_settings' => new SiteGeneralInfoFilterResource($site_settings),
             ]);
-        }else{
-            $site_settings = [
-                'com_site_title' => com_option_get('com_site_title'),
-                'com_site_subtitle' => com_option_get('com_site_subtitle'),
-                'com_site_favicon' => com_option_get('com_site_favicon'),
-                'com_site_logo' => com_option_get('com_site_logo'),
-                'com_site_footer_copyright' => com_option_get('com_site_footer_copyright'),
-                'com_site_email' => com_option_get('com_site_email'),
-                'com_site_website_url' => com_option_get('com_site_website_url'),
-                'com_site_contact_number' => com_option_get('com_site_contact_number'),
-                'com_site_full_address' => com_option_get('com_site_full_address'),
-                'com_maintenance_mode' => com_option_get('com_maintenance_mode'),
-                'com_user_login_otp' => com_option_get('com_user_login_otp'),
-                'com_user_email_verification' => com_option_get('com_user_email_verification'),
-            ];
         }
+
+        $site_settings = [
+            'com_site_title' => com_option_get('com_site_title'),
+            'com_site_subtitle' => com_option_get('com_site_subtitle'),
+            'com_site_favicon' => com_option_get('com_site_favicon'),
+            'com_site_logo' => com_option_get('com_site_logo'),
+            'com_site_footer_copyright' => com_option_get('com_site_footer_copyright'),
+            'com_site_email' => com_option_get('com_site_email'),
+            'com_site_website_url' => com_option_get('com_site_website_url'),
+            'com_site_contact_number' => com_option_get('com_site_contact_number'),
+            'com_site_full_address' => com_option_get('com_site_full_address'),
+            'com_maintenance_mode' => com_option_get('com_maintenance_mode'),
+            'com_user_login_otp' => com_option_get('com_user_login_otp'),
+            'com_user_email_verification' => com_option_get('com_user_email_verification'),
+        ];
+
 
         return response()->json([
             'site_settings' => New SiteGeneralInfoResource($site_settings),
