695aec5dc835d88055f3c5b82c1ef6e5bb74448c
Faysal <jesan@disystem.net>
2025-04-15 18:16:56 +0600

    MODIFIED: SellerProductManageController

--- app/Http/Controllers/Api/V1/Product/ProductAttributeController.php
+++ app/Http/Controllers/Api/V1/Product/ProductAttributeController.php
@@ -46,7 +46,7 @@ class ProductAttributeController extends Controller
             });
         }
         // Apply sorting and pagination
-        $attributes = $attributes->where('created_by',auth('api')->id())
+        $attributes = $attributes
             ->with(['related_translations', 'attribute_values'])
             ->orderBy($request->sortField ?? 'id', $request->sort ?? 'asc')
             ->paginate($limit);
