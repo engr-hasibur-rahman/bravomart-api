9baeab36eaaaa8bc52d298ee64bee08210839440
Faysal <jesan@disystem.net>
2025-04-16 13:42:48 +0600

    Merge

--- database/seeders/PermissionStoreSeeder.php
+++ database/seeders/PermissionStoreSeeder.php
@@ -446,7 +446,7 @@ class PermissionStoreSeeder extends Seeder
                 // Notifications Settings
                 [
                     'PermissionName' => '',
-                    'PermissionTitle' => 'Notifications',
+                    'PermissionTitle' => 'Notifications Management',
                     'activity_scope' => 'store_level',
                     'icon' => 'MessageCircleMore',
                     'options' => ['View'],
