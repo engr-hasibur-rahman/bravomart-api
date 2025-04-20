35bc97e48ee370c79f76a401dac178a3c404fdff
Faysal <jesan@disystem.net>
2025-04-20 12:22:17 +0600

    MODIFIED: StoreManageService

--- app/Services/StoreManageService.php
+++ app/Services/StoreManageService.php
@@ -27,64 +27,65 @@ class StoreManageService
 
         $data = Arr::except($data, ['translations']);
         $data['store_seller_id'] = auth('api')->id();
-        DB::transaction(function () use ($data) {
-            $store = Store::create($data);
+        DB::beginTransaction();
+        $store = Store::create($data);
 
-            // store create after commission set
-            $store = Store::find($store->id);
-            $store_id = $store->id;
-            // check commission system_commission settings
-            $systemCommission = SystemCommission::first();
-            $commission_enabled = $systemCommission->commission_enabled;
-            $subscription_enabled = $systemCommission->subscription_enabled;
-            if ($commission_enabled) {
-                if (isset($store->subscription_type) && $store->subscription_type === 'commission') {
-                    // get system commission
-                    $system_commission = SystemCommission::first();
+        // store create after commission set
+        $store = Store::find($store->id);
+        $store_id = $store->id;
+        // check commission system_commission settings
+        $systemCommission = SystemCommission::first();
+        $commission_enabled = $systemCommission->commission_enabled;
+        $subscription_enabled = $systemCommission->subscription_enabled;
+        if ($commission_enabled) {
+            if (isset($store->subscription_type) && $store->subscription_type === 'commission') {
+                // get system commission
+                $system_commission = SystemCommission::first();
 
-                    // Ensure the commission type is valid
-                    $commission_type = $system_commission->commission_type;
-                    if ($commission_type !== 'commission' && $commission_type !== 'fixed') {
-                        $commission_type = 'commission'; // Default value
-                    }
-
-                    // Update store commission
-                    $store->admin_commission_type = $commission_type;
-                    $store->admin_commission_amount = $system_commission->commission_amount;
-                    $store->save();
-
-                    // Dispatch job to send email in the background
-                    try {
-                        dispatch(new SendStoreCreatedEmailJob($store));
-                    } catch (\Exception $exception) {
-                    }
-                    return [
-                        'store_id' => $store_id,
-                        'slug' => $store->slug,
-                    ];
+                // Ensure the commission type is valid
+                $commission_type = $system_commission->commission_type;
+                if ($commission_type !== 'commission' && $commission_type !== 'fixed') {
+                    $commission_type = 'commission'; // Default value
                 }
-            } else {
-                return response()->json([
-                    'message' => __('messages.commission_option_is_not_available')
-                ],422);
-            }
-            // Store Subscription handle logic
-            // check subscription system_commission settings
-            if ($subscription_enabled){
-                if (moduleExistsAndStatus('Subscription')) {
-                    $this->subscriptionService->buySubscriptionPackage($store, $data);
-                }
-            } else {
-                return response()->json([
-                    'message' => __('messages.subscription_option_is_not_available')
-                ]);
-            }
-            return [
-                'store_id' => $store_id,
-                'slug' => $store->slug,
-            ];
-        });
 
+                // Update store commission
+                $store->admin_commission_type = $commission_type;
+                $store->admin_commission_amount = $system_commission->commission_amount;
+                $store->save();
+                DB::commit();
+                // Dispatch job to send email in the background
+                try {
+                    dispatch(new SendStoreCreatedEmailJob($store));
+                } catch (\Exception $exception) {
+                }
+                return [
+                    'store_id' => $store_id,
+                    'slug' => $store->slug,
+                ];
+            }
+        } else {
+            DB::rollBack();
+            return response()->json([
+                'message' => __('messages.commission_option_is_not_available')
+            ],422);
+        }
+        // Store Subscription handle logic
+        // check subscription system_commission settings
+        if ($subscription_enabled){
+            if (moduleExistsAndStatus('Subscription')) {
+                $this->subscriptionService->buySubscriptionPackage($store, $data);
+            }
+            DB::commit();
+        } else {
+            DB::rollBack();
+            return response()->json([
+                'message' => __('messages.subscription_option_is_not_available')
+            ]);
+        }
+        return [
+            'store_id' => $store_id,
+            'slug' => $store->slug,
+        ];
     }
 
 
