133d482b51ef540b23f0151ef25d0964ce85362f
Faysal <jesan@disystem.net>
2025-05-05 12:45:09 +0600

    MODIFIED: NoticeManageRepository

--- app/Http/Resources/Seller/SellerNoticeResource.php
+++ app/Http/Resources/Seller/SellerNoticeResource.php
@@ -14,9 +14,15 @@ class SellerNoticeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // Get the requested language from the query parameter
+        $language = $request->input('language', 'en');
+        // Get the translation for the requested language
+        $translation = $this->notice?->related_translations->where('language', $language);
         return [
             "notice_id" => $this->notice?->id,
-            "title" => $this->notice?->title,
+            "title" => !empty($translation) && $translation->where('key', 'title')->first()
+                ? $translation->where('key', 'title')->first()->value
+                : $this->notice?->title, // If language is empty or not provided attribute
             "type" => $this->notice?->type,
             "priority" => $this->notice?->priority,
             "status" => $this->notice?->status,
