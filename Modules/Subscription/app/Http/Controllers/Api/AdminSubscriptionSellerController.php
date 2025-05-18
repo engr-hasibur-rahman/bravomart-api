468459dbee3c0cf8661ae6c00dfa01601c37eb55
Faysal <jesan@disystem.net>
2025-05-18 10:12:26 +0600

    MODIFIED: AdminSubscriptionSellerController

--- Modules/Subscription/app/Http/Controllers/Api/AdminSubscriptionSellerController.php
+++ Modules/Subscription/app/Http/Controllers/Api/AdminSubscriptionSellerController.php
@@ -25,20 +25,31 @@ class AdminSubscriptionSellerController extends Controller
     public function index(Request $request)
     {
         $query = StoreSubscription::query();
+
         if (!empty($request->search)) {
-            $query->where('name', 'LIKE', '%' . $request->search . '%');
+            $query->where(function ($q) use ($request) {
+                $q->where('name', 'LIKE', '%' . $request->search . '%')
+                    ->orWhereHas('store', function ($storeQuery) use ($request) {
+                        $storeQuery->where('name', 'LIKE', '%' . $request->search . '%');
+                    });
+            });
         }
         if (isset($request->status)) {
             $query->where('status', $request->status ?? 1);
         }
 
-        if ($request->has('expire_date') && !empty($request->expire_date)) {
-            $query->whereDate('expire_date', $request->expire_date);
+        if ($request->has('start_date') && !empty($request->start_date) && $request->has('end_date') && !empty($request->end_date)) {
+            $query->whereBetween('created_at', [$request->start_date, $request->end_date]);
         }
 
-        if ($request->has('created_at') && !empty($request->created_at)) {
-            $query->whereDate('created_at', $request->created_at);
+        if ($request->has('start_expire_date') && !empty($request->start_expire_date) && $request->has('end_expire_date') && !empty($request->end_expire_date)) {
+            $query->whereBetween('expire_date', [$request->start_expire_date, $request->end_expire_date]);
         }
+
+        if (isset($request->store_id)) {
+            $query->where('store_id', $request->store_id);
+        }
+
         $subscriptions = $query->paginate(10);
         return response()->json([
             'success' => true,
