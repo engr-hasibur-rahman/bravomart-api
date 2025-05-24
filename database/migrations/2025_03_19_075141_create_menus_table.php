9bb5069b17f5f32266702049492c00884381fe91
Hasibur <hasibur@disystem.net>
2025-05-24 17:47:52 +0600

    Added menu with page added

--- database/migrations/2025_03_19_075141_create_menus_table.php
+++ database/migrations/2025_03_19_075141_create_menus_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('page_id')->nullable();
             $table->string('name');
             $table->string('url')->nullable();
             $table->string('icon')->nullable();
@@ -22,7 +23,7 @@ return new class extends Migration
             $table->integer('menu_level')->nullable();           // Optional, similar to category_level
             $table->string('menu_path')->nullable();             // Optional, similar to parent_path
             $table->boolean('is_visible')->default(true);
-            $table->integer('status')->default(1);
+            $table->integer('status')->default(1); // if status 1 editable and if 0  (seeder apply) not editable
             $table->timestamps();
         });
     }
