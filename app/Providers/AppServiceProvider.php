b42f404cb96f931a0d0298c1ab055500445a9d02
Hasibur <hasibur@disystem.net>
2025-05-31 11:49:38 +0600

    chat route issue fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,6 +34,7 @@ class AppServiceProvider extends ServiceProvider
         Store::observe(SellerStoreWiseObserver::class);
         Store::observe(StoreObserver::class);
         Order::observe(OrderObserver::class);
+
         // relationship add
         Relation::morphMap([
             'customer'     => Customer::class,
