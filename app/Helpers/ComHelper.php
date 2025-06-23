3f937ab7a3af3bd8715a9a5dfa33e49873ac921a
Hasibur <hasibur@disystem.net>
2025-06-23 16:56:20 +0600

    otp service create

--- app/Helpers/ComHelper.php
+++ app/Helpers/ComHelper.php
@@ -11,26 +11,6 @@ use App\Models\Media;
 class ComHelper
 {
 
-    /**
-     * remove_invalid_charcaters
-     * Remove unaccepteable charecters from the text
-     *
-     *
-     * @param mixed $str
-     * @return void
-     */
-    public static function remove_invalid_charcaters($str)
-    {
-        return str_ireplace(['\'', '<', '>'], '"', ';', ' ', $str);
-    }
-
-    /**
-     * format_coordiantes
-     * Format geometry coordiantes to make it useable for front-end
-     *
-     * @param mixed $coordinates
-     * @return void Coordinte Array
-     */
     public static function format_coordiantes($coordinates)
     {
         $data = [];
@@ -40,20 +20,10 @@ class ComHelper
         return $data;
     }
 
-
-    /**
-     * get_com_settings
-     * get settings otpion from database
-     *
-     * @param mixed $name Name of the Option
-     * @return string Json Decoded string
-     */
     public static function get_com_settings($name)
     {
         $config = null;
-
         $paymentmethod = SettingOption::where('option_name', $name)->first();
-
         if ($paymentmethod) {
             $config = json_decode($paymentmethod->value, true);
         }
@@ -61,51 +31,24 @@ class ComHelper
         return $config;
     }
 
-    /**
-     * getDirectory
-     * Get Direcotry Name
-     *
-     * @return void
-     */
     public static function getDirectory()
     {
         $config = ComHelper::get_com_settings('local_storage');
-
         return isset($config) ? ($config == 0 ? 's3' : 'public') : 'public';
     }
 
-    /**
-     * get_image_base_url
-     * Get Base Url for image to Show
-     * @return void
-     */
     public static function get_image_base_url()
     {
         //For Dynamic URL. Like AWS/CloudFront/Local
         return env('APP_URL') . '/storage';
     }
 
-
-    /**
-     * Upload Single document to folder
-     *
-     * @param string $dir Direcotry name to Save the File
-     * @param object $file Image Object
-     * @param string $old_image Provide old image name to replaec, if data is updating
-     * @param string $format File format/extension, if you want to change file in different format
-     * @return string Image Name only
-     */
     public static function uploadSingle(string $dir, $file, string $old_image = null, string $format = null): string
     {
         try {
-
-
             if ($old_image != null) {
-                // Olde Image To Delete
                 $file_path = $dir . '/' . $old_image;
-                //Check if Exists
                 if (Storage::disk(self::getDirectory())->exists($file_path)) {
-                    // Old Image Exists, now delete It
                     Storage::disk(self::getDirectory())->delete($file_path);
                 }
             }
@@ -113,9 +56,7 @@ class ComHelper
             // Upload File
             if ($file != null) {
                 $imageName = \Carbon\Carbon::now()->toDateString() . "-" . uniqid() . "." . $file->extension();
-                // Check Folder Exists
                 if (!Storage::disk(self::getDirectory())->exists($dir)) {
-                    // Folder not found Create It
                     Storage::disk(self::getDirectory())->makeDirectory($dir);
                 }
                 Storage::disk(self::getDirectory())->putFileAs($dir, $file, $imageName);
@@ -127,30 +68,14 @@ class ComHelper
         return $imageName;
     }
 
-
-    /**
-     * Upload Multiple document to folder
-     *
-     * @param string $dir Direcotry name to Save the File
-     * @param object $file array of Image Object
-     * @param string $old_image Provide old image name to replaec, if data is updating
-     * @param string $format File format/extension, if you want to change file in different format
-     * @return string Image Name Array
-     */
     public static function uploadMulti(string $dir, array $files, array $old_images = null, string $format = null): array
     {
         $fileNames = [];
         try {
-
-            //Delete all Existing Image
             foreach ($old_images as $old_image) {
-
                 if ($old_image != null) {
-                    // Olde Image To Delete
                     $file_path = $dir . '/' . $old_image;
-                    //Check if Exists
                     if (Storage::disk(self::getDirectory())->exists($file_path)) {
-                        // Old Image Exists, now delete It
                         Storage::disk(self::getDirectory())->delete($file_path);
                     }
                 }
@@ -158,12 +83,9 @@ class ComHelper
 
             // Upload files one by One
             foreach ($files as $file) {
-
                 if ($file != null) {
                     $fileName = \Carbon\Carbon::now()->toDateString() . "-" . uniqid() . "." . $file->extension();
-                    //Check if Exists
                     if (!Storage::disk(self::getDirectory())->exists($dir)) {
-                        // Folder not found Create It
                         Storage::disk(self::getDirectory())->makeDirectory($dir);
                     }
                     Storage::disk(self::getDirectory())->putFileAs($dir, $file, $fileName);
@@ -174,7 +96,6 @@ class ComHelper
             }
         } catch (\Exception $e) {
         }
-
         return $fileNames;
     }
 
@@ -182,16 +103,12 @@ class ComHelper
     public static function buildMenuTree(array $role_id, $data_list)
     {
         $tree = [];
-//        dd($data_list->toArray());
         foreach ($data_list as $data_item) {
-//            dump($data_item->children->toArray());
-//            $children = $data_item->children != '' && count($data_item->children) ? ComHelper::buildMenuTree($role_id, $data_item->children) : [];
             $children = $data_item->childrenRecursive != '' && count($data_item->childrenRecursive)
                 ? ComHelper::buildMenuTree($role_id, $data_item->childrenRecursive)
                 : [];
             $users = DB::table('role_has_permissions')->where('permission_id', $data_item->id)->whereIn('role_id', $role_id)->first();
             $translations = Translation::where('translatable_type', 'App\Models\Permissions')->where('translatable_id', $data_item->id)->get()->groupBy('language');
-            //logger($translations);
             $transformedData = [];
             foreach ($translations as $language => $items) {
                 $itemData = [
@@ -202,28 +119,8 @@ class ComHelper
             }
 
             $options = [];
-//            if($users) {
-//                $options = array_map(function ($allowedValue) use ($users) {
-//                    return [
-//                        'label' => $allowedValue,
-//                        'value' => (bool) ($users->$allowedValue ?? false), // Use null if the property doesn't exist
-//                    ];
-//                },
-//                    json_decode($data_item->options, true)
-//                ); // Decode as associative array
-//            }else{
-//                $options = array_map(function ($allowedValue) use ($users) {
-//                    return [
-//                        'label' => $allowedValue,
-//                        'value' => false
-//                    ];
-//                }, json_decode($data_item->options, true)); // Decode as associative array
-//            }
-
-            $options = [];
             $decodedOptions = $data_item->options;
-
-// Check and decode options
+            // Check and decode options
             if (is_string($decodedOptions)) {
                 $decodedOptions = json_decode($decodedOptions, true);
 
@@ -236,10 +133,10 @@ class ComHelper
                 }
             }
 
-// Ensure decoded options is an array
+           // Ensure decoded options is an array
             if (is_array($decodedOptions)) {
                 $options = array_map(function ($allowedValue) use ($users) {
-                    if (is_string($allowedValue)) { // Only process if it's a string
+                    if (is_string($allowedValue)) {
                         return [
                             'label' => $allowedValue,
                             'value' => $users && property_exists($users, $allowedValue)
@@ -252,17 +149,14 @@ class ComHelper
                     logger('Invalid allowed value type:', ['allowedValue' => $allowedValue]);
                     return null; // Skip invalid values
                 }, $decodedOptions);
-
                 // Remove null values caused by invalid items
                 $options = array_filter($options);
             } else {
                 logger('Decoded options is not an array or is invalid:', ['decodedOptions' => $decodedOptions]);
             }
 
-
             $tree[] = [
                 'id' => $data_item->id,
-                //'is_assigned' =>(bool) ($data_item->is_assigned?? false),
                 'perm_title' => $data_item->perm_title,
                 'perm_name' => $data_item->name,
                 'type' => $data_item->type ?? null,
