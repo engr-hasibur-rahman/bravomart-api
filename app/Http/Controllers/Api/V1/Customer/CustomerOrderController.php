c47cec78388a7c32965f2bda072d8c1176c86a08
Faysal <jesan@disystem.net>
2025-07-01 15:13:03 +0600

    MODIFIED: CustomerOrderController SellerStoreOrderController

--- app/Http/Controllers/Api/V1/Customer/CustomerOrderController.php
+++ app/Http/Controllers/Api/V1/Customer/CustomerOrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\V1\Controller;
 use App\Http\Resources\Com\Pagination\PaginationResource;
 use App\Http\Resources\Order\CustomerOrderResource;
 use App\Http\Resources\Order\InvoiceResource;
+use App\Http\Resources\Order\OrderRefundRequestResource;
 use App\Http\Resources\Order\OrderSummaryResource;
 use App\Models\CouponLine;
 use App\Models\Order;
@@ -32,7 +33,8 @@ class CustomerOrderController extends Controller
             return response()->json([
                 'order_data' => new CustomerOrderResource($order),
                 'order_summary' => new OrderSummaryResource($order),
-            ], 200);
+                'refund' => $order->refund ? new OrderRefundRequestResource($order->refund) : null,
+            ]);
         }
 
         $request['status'] = $request->status == 'active' ? 'confirmed' : $request->status;
