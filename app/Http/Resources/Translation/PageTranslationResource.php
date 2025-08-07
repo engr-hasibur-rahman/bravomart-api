6e3e7c4c67ca1a5236202a918be8f9547e57335a
Faysal <jesan@disystem.net>
2025-08-07 14:29:04 +0600

    MODIFIED: PageTranslationResource

--- app/Http/Resources/Translation/PageTranslationResource.php
+++ app/Http/Resources/Translation/PageTranslationResource.php
@@ -18,7 +18,7 @@ class PageTranslationResource extends JsonResource
         return [
             "language_code" => $this->first()->language,
             "title" => safeJsonDecode($this->where('key', 'title')->first()?->value) ,
-            "content" => safeJsonDecode($this->where('key', 'content')->first()?->value) ,
+            "content" => jsonImageModifierFormatter(safeJsonDecode($this->where('key', 'content')->first()?->value)),
             "meta_title" => safeJsonDecode($this->where('key', 'meta_title')->first()?->value) ,
             "meta_description" => safeJsonDecode($this->where('key', 'meta_description')->first()?->value) ,
             "meta_keywords" => safeJsonDecode($this->where('key', 'meta_keywords')->first()?->value) ,
